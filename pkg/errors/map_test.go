c69703c31d7cf642f52c53d4844eebdaf7020dba
sirily11 <32106111+sirily11@users.noreply.github.com>
2024-02-19 16:06:28 +0800

    fix: update error function (#7)

--- pkg/errors/map_test.go
+++ pkg/errors/map_test.go
@@ -1,6 +1,9 @@
 package errors
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestMapErrorCodeToHTTPStatus(t *testing.T) {
 	type args struct {
@@ -31,6 +34,35 @@ func TestMapErrorCodeToHTTPStatus(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := MapErrorCodeToHTTPStatus(tt.args.code); got != tt.want {
 				t.Errorf("MapErrorCodeToHTTPStatus() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMapErrorToHTTPStatus(t *testing.T) {
+	type args struct {
+		error error
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "MissingAPIKey",
+			args: args{error: NewDocumentNotFound()},
+			want: 404,
+		},
+		{
+			name: "OtherError",
+			args: args{error: fmt.Errorf("other error")},
+			want: 500,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MapErrorToHTTPStatus(tt.args.error); got != tt.want {
+				t.Errorf("MapErrorToHTTPStatus() = %v, want %v", got, tt.want)
 			}
 		})
 	}
